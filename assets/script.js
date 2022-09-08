d259aa7a9d8dfb2a81e0c9b8b69009992540f130
Your Name <you@example.com>
2022-09-08 19:27:53 +0900

    4-commit

--- assets/script.js
+++ assets/script.js
@@ -88,7 +88,7 @@ $('.gallery').on('beforeChange', function(event, slick, currentSlide, nextSlide)
 
 
 function toggleNav() {
-  var body = document.header;
+  var body = document.body;
   var hamburger = document.getElementById('js-hamburger');
    var blackBg = document.getElementById('js-black-bg');
 
